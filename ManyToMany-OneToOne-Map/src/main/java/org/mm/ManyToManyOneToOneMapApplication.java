3ca8614bd39a1dc3bb41eaea05bf66a90af0d3d6
ManPatel-07 <patelman7720@gmail.com>
2024-07-27 18:11:10 +0530

    Spring Boot Project Created With dependecies

---
+++ ManyToMany-OneToOne-Map/src/main/java/org/mm/ManyToManyOneToOneMapApplication.java
@@ -0,0 +1,13 @@
+package org.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManyOneToOneMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyToManyOneToOneMapApplication.class, args);
+	}
+
+}
